07256da547b436ee92bb5a6758ad152cb3a0f7ce
alvislim <luvhatehero1@gmail.com>
2024-05-20 11:39:22 +0800

    fixed url

--- src/url.js
+++ src/url.js
@@ -1,5 +1,5 @@
 // const url = 'http://localhost:4000'
 const url =
-  "https://final-project-be-git-master-alvis-projects-b1bc7917.vercel.app/";
+  "https://final-project-be-git-master-alvis-projects-b1bc7917.vercel.app";
 
 export default url;

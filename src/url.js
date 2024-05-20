0ae61546c53f40d560e7a7b0e6d85960a07ac304
alvislim <luvhatehero1@gmail.com>
2024-05-20 11:28:41 +0800

    update end point

--- src/url.js
+++ src/url.js
@@ -1,4 +1,5 @@
 // const url = 'http://localhost:4000'
-const url = "https://final-project-be-lime.vercel.app";
+const url =
+  "https://final-project-be-git-master-alvis-projects-b1bc7917.vercel.app/";
 
 export default url;
